5c3d04d7896fc25d9504798b338b58e2bec81a60
thiagoirch <thiagoirch@gmail.com>
2023-05-31 16:55:03 -0300

    Poller Finops

---
+++ finops.py
@@ -0,0 +1,162 @@
+import boto3
+import sys
+import datetime
+import argparse
+#from pyzabbix import ZabbixMetric, ZabbixSender
+########################
+#Variáveis
+########################
+zabbixServer = "zbxsrv.zbx.com.br"
+dateNow = datetime.datetime.now()
+daTe = dateNow.strftime("%Y-%m-%d:%H:%M")
+
+########################
+#Conexão AWS
+########################
+#Abre conexão
+def awsConnect(awsType):
+    session = boto3.Session(
+        region_name=reGion
+    )
+    awsClient = session.client(awsType)
+    return awsClient
+#Valida conta
+def awsAccount():
+    awsSession = awsConnect('sts')
+    response = awsSession.get_caller_identity()
+    accountId = response['Account']
+    return accountId
+
+###############
+#Loga o retorno
+###############
+def zabbix(accountId, feaTure, zEvent):
+    zServer = ZabbixSender(zabbixServer, 10051)
+    zPac = [ ZabbixMetric(accountId,str(f"{accountId}-{feaTure}"), zEvent)]
+    zServer.send(zPac)
+    print(f"{daTe} - Account:{accountId} - Region: {reGion} - Feature:{feaTure} - {mesSage}")
+def log(daTe, feaTure, mesSage, reGion, zEvent):
+    accountId = awsAccount()
+    print(f"{zEvent} - {daTe} - Account:{accountId} - Region: {reGion} - Feature:{feaTure} - {mesSage}")
+    if ouT == "zabbix":
+        zabbix(accountId, feaTure, zEvent)
+###############
+#Funções
+###############
+#Check Elastic IPs não associado
+def chkEip():
+    feaTure = "eip"
+    awsSession = awsConnect('ec2')
+    response = awsSession.describe_addresses(
+        Filters=[
+            {
+                'Name': 'domain',
+                'Values': ['vpc']
+            },
+        ]
+    )
+    zEvent = 0
+    feaTure = "eip"
+    for eip in response['Addresses']:
+        eip_id = eip['PublicIp']
+        ass_id = eip.get('AssociationId', 'Null')
+        if ass_id == 'Null':
+            mesSage = str(f"Addr: {eip_id} Status: Não Associado")
+            zEvent = zEvent + 1
+            log(daTe, feaTure, mesSage, reGion, zEvent)
+#Check Elastic Network Interface (eni) não atachado
+def chkEni():
+    feaTure = "eni"
+    awsSession = awsConnect('ec2')
+    response = awsSession.describe_network_interfaces()
+    unused_enis = []
+
+    for eni in response['NetworkInterfaces']:
+        if 'Attachment' not in eni:
+            unused_enis.append(eni)
+    zEvent = 0
+    feaTure = "eni"
+    for eni in unused_enis:
+        eni_id = eni['NetworkInterfaceId']
+        mesSage = str(f"Network Interface: {eni_id} Status: Não Associada")
+        zEvent = zEvent + 1
+        log(daTe, feaTure, mesSage, reGion, zEvent)
+#Check EBS não atachado
+def chkEbs():
+    feaTure = "ebs"
+    awsSession = awsConnect('ec2')
+    response = awsSession.describe_volumes(
+        Filters=[
+                {
+                    'Name': 'status',
+                    'Values': ['available']
+                }
+            ]
+        )
+    zEvent = 0
+    feaTure = "ebs"
+    for volume in response['Volumes']:
+        volume_id = volume['VolumeId']
+        volume_status = volume['State']
+        zEvent = zEvent + 1
+        mesSage = str(f"Volume: {volume_id} Status:{volume_status}")
+        log(daTe, feaTure, mesSage, reGion, zEvent)
+#Checa Target Groups sem target registrado
+def chkTgt():
+    feaTure = "tgt"
+    awsSession = awsConnect('elbv2')
+    response = awsSession.describe_target_groups()
+    zEvent = 0
+    for target_group in response['TargetGroups']:
+        target_group_arn = target_group['TargetGroupArn']
+        target_group_name = target_group['TargetGroupName']
+        
+        response_instances = awsSession.describe_target_health(
+            TargetGroupArn=target_group_arn
+        )
+        
+        target_instances = response_instances['TargetHealthDescriptions']
+        
+        if not target_instances:
+            mesSage = str(f"Target Group: {target_group_name} Status: Sem target registrado")
+            zEvent = zEvent + 1
+            log(daTe, feaTure, mesSage, reGion, zEvent)
+
+
+######################
+#Argumentos
+######################
+arguMent = argparse.ArgumentParser(description='Valores para checagem')
+arguMent.add_argument('--feature', type=str, default="all", help='Declare a feature que deseja. Default:all.')
+arguMent.add_argument('--region', type=str, default="us-east-1", help='Declare a região que deseja consultar. Default:us-east-1.')
+arguMent.add_argument('--out', type=str, default="log", help='Declare aqui o modo de saída padrão [log|zabbix]. Default:log.')
+args = arguMent.parse_args()
+feaTure = args.feature
+reGion = args.region
+ouT= args.out
+######################
+#Aciona as funções
+######################
+#
+#feature
+def procopt(feaTure):
+    if feaTure == "ebs":
+        chkEbs()
+    elif feaTure == "eni":
+        chkEni()
+    elif feaTure == "eip":
+        chkEip()
+    elif feaTure == "tgt":
+        chkTgt()
+    elif feaTure == "all":
+        chkEbs()
+        chkEip()
+        chkEni()
+        chkTgt()
+    else:
+        print(f"Feature inválida.")
+
+####################
+#Chamada principal
+####################
+procopt(feaTure)
